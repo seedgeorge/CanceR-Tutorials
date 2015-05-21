31bf33ab2bc4445cba61a06eb77cf7b32e343977
GSeed <seedgeorge@gmail.com>
2015-05-21 15:42:11 +0100

    finished section 0

--- June_2015_intro/All_code.R
+++ June_2015_intro/All_code.R
@@ -1,8 +1,14 @@
 # Gene Expression analysis in R
 
-# Section 0 - packages and libraries
+## Section 0 - packages and libraries
+
 # later on we might need some extra tools and systems
 # so we'll download them now - select the lines below, and use Edit->Execute to run them
+# remember, everything prefaced by the symbol '#' is a comment, and is purely for making notes in the code
+
+# if any of them ask whether you want to update other packages, press 'a' for all
+# and if any ask you if they should be put into other directories, press 'y' for yes
+
 source("http://bioconductor.org/biocLite.R")
 
 biocLite("Biobase")
@@ -20,3 +26,22 @@ library(survival)
 library(oligo)
 library(affy)
 library(annotate)
+
+
+## Section 1 - we have to do a little bit of R basics
+# we will access the data from http://www.nejm.org/doi/full/10.1056/NEJMoa021967
+# it is a microarray gene expression dataset of 295 breast cancers
+# first we will read the data into R from a file
+# this command looks for a specific file and makes it into a table that R can deal with
+NKI295_data <- read.table("data/NKI295.expression.data.txt", sep="\t", header=T)
+
+#	what kind of data is this?
+class(NKI295_data)
+summary(NKI295_data)
+
+#	what is the difference between a data frame and a matrix?
+#	what kind of data is in each column?
+apply(NKI295_data, 2, class)
+
+
+
