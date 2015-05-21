01a25b511258c610502fcdd70ffcb19dae9f8d9f
GSeed <seedgeorge@gmail.com>
2015-05-21 17:14:00 +0100

    up to 'optional tasks'

--- June_2015_intro/All_code.R
+++ June_2015_intro/All_code.R
@@ -17,6 +17,7 @@ biocLite("survival")
 biocLite("oligo")
 biocLite("affy")
 biocLite("annotate")
+biocLite("gplots")
 
 library(Biobase)
 library(marray)
@@ -25,43 +26,54 @@ library(survival)
 library(oligo)
 library(affy)
 library(annotate)
+library(gplots)
 
 ########################################################
 ## Section 1 - we have to do a little bit of R basics
 # we will access the data from http://www.nejm.org/doi/full/10.1056/NEJMoa021967
 # it is a microarray gene expression dataset of 295 breast cancers
 # first we will read the data into R from a file
-# this command looks for a specific file and makes it into a table that R can deal with
-NKI295_data <- read.table("data/NKI295.expression.data.txt", sep="\t", header=T)
-
-# what kind of data is this?
-# the following commands will give you clues about the data without opening it to read
-class(NKI295_data)
-summary(NKI295_data)
-nrow(NKI295_data)
-ncol(NKI295_data)
-
-# what is the difference between a data frame and a matrix?
-# what kind of data is in each column?
-head(NKI295_data)
-apply(NKI295_data, 2, class)
-
-# lets make a quick plot of all the scores 
-# we will use the boxplot command, and select all the columns except the first six
-boxplot(NKI295_data[,-1:-6], pch=16, cex=0.1)
-
-# but we also want to know which sample is which and what their phenotypes are
-# so we will read in some more data
-NKI295_samples <- read.table("data/NKI295.samples.txt", sep="\t", header=T)
-class(NKI295_samples)
-head(NKI295_samples)
-# we have to annotate the samples in the dataset and the genes on the array...
-
-# to do this elegantly we can use a more complicated data structure called an expression set
-# similar to the 'read.table' function we used before, we'll use  'readExpressionSet'
+# we will use an appropriate function to read this data - readExpressionSet
 NKI295 <- readExpressionSet(exprsFile="data/NKI295.exprs.txt", sep="\t", header=T, stringsAsFactors=F) 
 
-# we'll include some extra data (WHAT ARE THESE)
+# now, the object 'NKI295' is a data structure holding the expression data
+
+# we'll include some extra data 
 fData(NKI295) <- read.delim("data/NKI295.fdata.txt", sep="\t", header=T, row.names=1, stringsAsFactors=F) 
 pData(NKI295) <- read.table("data/NKI295.pdata.txt", sep="\t", header=T, row.names=1) 
+# fData is information about the probes
+# pData is information about the samples
+# these are now associated with the expression set
+
+# we will first look at the values for ESR1
+exprs(NKI295)[which(fData(NKI295)$symbol == "ESR1"),]
+
+# lets make some plots!
+# a barplot is a solid way to start visualising this
+barplot(exprs(NKI295)[which(fData(NKI295)$symbol == "ESR1"),])
+
+# we can clean it up a little with some extra parameters
+barplot(exprs(NKI295)[which(fData(NKI295)$symbol == "ESR1"),], cex.names=0.2, las=2)
+
+# and make it more interesting with some colours
+barplot(exprs(NKI295)[which(fData(NKI295)$symbol == "ESR1"),], cex.names=0.1, las=2, col=subtype.colours)
+
+# it's a little confusing, so lets sort the bars to make a waterfall plot
+NKI295 <- NKI295[,order(exprs(NKI295)[which(fData(NKI295)$symbol == "ESR1"),])]
+barplot(exprs(NKI295)[which(fData(NKI295)$symbol == "ESR1"),], cex.names=0.2, las=2, col=subtype.colours[NKI295$subtype], main="ER expression by sample")
+
+# lets save this plot into the data folder, it will be named 'subtype.ER.waterfall.pdf'
+pdf("subtype.ER.waterfall.pdf", width=12, height=6)
+barplot(exprs(NKI295)[which(fData(NKI295)$symbol == "ESR1"),], cex.names=0.2, las=2, col=subtype.colours[NKI295$subtype], main="ER expression by sample")
+legend("bottomright", legend=levels(NKI295$subtype), fill=subtype.colours, bty="n")
+dev.off()
+
+# let's do something similar but use a boxplot instead
+pdf("subtype.ER.boxplot.pdf")
+boxplot(exprs(NKI295)[which(fData(NKI295)$symbol == "ESR1"),]~NKI295$subtype, col=subtype.colours, main="ER expression by subtype")
+dev.off()
+
+## End of section 1
+# Optional tasks!
+# Try plotting a different gene - not ESR1 - 
 
