45f82990e991aadfe756e2288bedcd05ba55189e
GSeed <seedgeorge@gmail.com>
2015-05-21 14:55:21 +0100

    Refactoring and cleaning up code
    Also adding extensive comments.

--- June_2015_intro/All_code.R
+++ June_2015_intro/All_code.R
@@ -1,1 +1,10 @@
-#Some code here
+# Gene Expression analysis in R
+
+# Section 0 - packages and libraries
+# later on we might need some extra tools and systems
+# so we'll download them now - select the lines below, and use Edit->Execute to run them
+source("http://bioconductor.org/biocLite.R")
+biocLite("Biobase")
+biocLite("marray")
+biocLite("limma")
+
