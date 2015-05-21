bb09d78e952fdd1bd28abd142be34d8c75407981
GSeed <seedgeorge@gmail.com>
2015-05-21 15:09:45 +0100

    Libraries

--- June_2015_intro/All_code.R
+++ June_2015_intro/All_code.R
@@ -4,7 +4,19 @@
 # later on we might need some extra tools and systems
 # so we'll download them now - select the lines below, and use Edit->Execute to run them
 source("http://bioconductor.org/biocLite.R")
+
 biocLite("Biobase")
 biocLite("marray")
 biocLite("limma")
+biocLite("survival")
+biocLite("oligo")
+biocLite("affy")
+biocLite("annotate")
 
+library(Biobase)
+library(marray)
+library(limma)
+library(survival)
+library(oligo)
+library(affy)
+library(annotate)
